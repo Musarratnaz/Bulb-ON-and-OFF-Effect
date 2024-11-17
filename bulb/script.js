119f0db36398e7874fdd18277918b283850504eb
Musarrat Naz <147928780+Musarratnaz@users.noreply.github.com>
2024-11-18 00:15:42 +0500

    Add files via upload

---
+++ bulb/script.js
@@ -0,0 +1,23 @@
+let bulb= document.getElementById("bulb");
+function bulbon(){
+    bulb.src ="glowing-bulb.png";
+    document.getElementById("btn1").style.background = "black";
+    document.getElementById("btn2").style.background = "rgb(236,71,0)";
+
+
+}
+function bulboff(){
+    bulb.src ="bulb-off-removebg-preview.png";
+    document.getElementById("btn2").style.background = "black";
+    document.getElementById("btn1").style.background = "rgb(236,71,0)";
+
+}
+
+
+
+
+
+
+
+
+
